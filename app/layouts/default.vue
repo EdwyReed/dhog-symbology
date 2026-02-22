70cea38defb6131f87e0e427c910d43469cb27ab
Edward Reed <edward@rolep.wok>
2026-02-22 14:50:48 +0200

    Added footer

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col">
     <Header />
-    <main class="container mx-auto px-4 md:px-6 lg:px-8 pb-20">
+    <main class="container mx-auto px-4 md:px-6 lg:px-8 pb-20 flex-grow">
       <slot />
     </main>
+    <CabinetFooter />
   </div>
 </template>
 
